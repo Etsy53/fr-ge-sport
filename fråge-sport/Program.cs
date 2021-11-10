669a9053e16f19eaa27843d842a191c36d41823e
Kasper Javelin <kasper.javelin@mail.com>
2021-11-10 12:30:07 +0100

    fråge sport

---
+++ fråge-sport/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+Console.WriteLine("vad är 1+1");
+string answer = Console.ReadLine();
+int points = 0;
+if (answer == "2")
+{
+    Console.WriteLine("Du hade RÄTT!!!");
+    Console.WriteLine("Grattis Du Fick 1 Poäng");
+    points = points + 1;
+    Console.ReadLine();
+}
+
+
+Console.WriteLine("vad är 5x5");
+answer = Console.ReadLine();
+if (answer == "25")
+{
+    Console.WriteLine("Du hade RÄTT!!!");
+    Console.WriteLine("Grattis Du Fick 1 Poäng");
+    points = points + 1;
+    Console.ReadLine();
+}
+
+Console.WriteLine("vad är 10x10");
+answer = Console.ReadLine();
+if (answer == "100")
+{
+    Console.WriteLine("Du hade RÄTT!!!");
+    Console.WriteLine("Grattis Du Fick 1 Poäng");
+    points = points + 1;
+    Console.ReadLine();
+}
+
+Console.WriteLine($"grattis, du fick {points} poäng");
+
+Console.ReadLine();
